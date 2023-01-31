b3c46d2b147eaa99b0ea5fe72e44b0846f7f19ef
mkhoma-appwell <mkhoma.appwell@ancestry.com>
2023-01-31 23:54:32 +0200

    [lesson1]: Added implementation of advanced task

---
+++ lesson1/main.js
@@ -0,0 +1,58 @@
+{
+    let hello = 'hello';
+    console.log(hello);
+
+    let owu = 'owu';
+    console.log(owu);
+
+    let com = 'com';
+    console.log(com);
+
+    let ua = 'ua';
+    console.log(ua);
+
+    let a = 1;
+    console.log(a);
+
+    let b = 10;
+    console.log(b);
+
+    let c = -999;
+    console.log(c);
+
+    let d = 123;
+    console.log(d);
+
+    let pi = 3.14;
+    console.log(pi);
+
+    let fl = 2.7;
+    console.log(fl);
+
+    let e = 16;
+    console.log(e);
+
+    let yes = true;
+    console.log(yes);
+
+    let no = false;
+    console.log(no);
+}
+{
+    let name = 'Marta';
+    let surname = 'Khoma';
+    let fathername = "Andriyivna";
+    let pib = name + " " + surname + " " + fathername;
+    let pib1 = `${name} ${surname} ${fathername}`;
+    console.log(pib);
+    console.log(pib1);
+}
+{
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+
